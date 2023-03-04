7cace2a53a75114e4920f1d726292216fb280dcc
wojcdyda <wojciech.dydacki@tele2.com>
2023-03-04 21:19:22 +0100

    feat: implement base css for the entire app

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,7 +3,7 @@
     <div class=" navbar fixed flex justify-start top-0 w-full z-500 whitespace-nowrap"
     @click.left.stop>
       <div class="flex items-center select-none">
-        <div class="logo flex items-start font-bold ">
+        <div class="logo flex items-start font-bold ml-4 mr-4">
           FRIDAY FINANCE
         </div>
         <div class="router-link flex items-center cursor-pointer">
@@ -30,10 +30,7 @@
 
 .logo {
   color: var(--color-text);
-  margin-left: 16px;
-  margin-right: 16px;
   font-size: 20px;
-  margin-top: -3px;
 }
 
 .router-link {
